9f979de3ae1135fd44672a9271317851022167d1
ShivamSoniss27 <96885334+ShivamSoniss27@users.noreply.github.com>
2022-12-10 14:19:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+  // creating a template of car
+  class car(){
+    public void run(){
+      System.out.print("Car is running");
+    }
+  }
+  // creating a company tag which is extending the car class
+  class tata extends car{
+    // overriding run method 
+    pubic void run(){
+      System.out.print("Tata car is running");
+    }
+  }
 }
